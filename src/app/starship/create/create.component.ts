649c2b0d74710b0b572c02514e10c59b12f9a16a
Pablo S <pablosmr7@gmail.com>
2022-05-05 11:39:02 +0200

    Indice 33% + crear Nave 50%

--- src/app/starship/create/create.component.ts
+++ src/app/starship/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StarshipService } from '../starship.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    public starshipService: StarshipService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
+    this.form = new FormGroup({
+      name:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ0-9 \-\']+') ]),
+      credits: new FormControl('', [ Validators.pattern("^[0-9]*$") ])
+      //phone: new FormControl('', [ Validators.required, Validators.pattern("^[0-9]*$") ])
+    });
+
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+    this.starshipService.create(this.form.value).subscribe(res => {
+         console.log('La nave se ha registrado exitosamente');
+         this.router.navigateByUrl('starship/index');
+    })
   }
 
 }
--- src/app/starship/index/index.component.ts
+++ src/app/starship/index/index.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StarshipService } from '../starship.service';
+import { Starship } from '../starship';
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  starships: Starship[] = [];
+
+  //constructor() { }  //Original
+  constructor(public starshipService: StarshipService) { }
 
   ngOnInit(): void {
+    this.starshipService.getAll().subscribe((data: Starship[])=>{
+      this.starships = data;
+      console.log(this.starships);
+    })
+  }
+
+  deleteStarship(id){
+    this.starshipService.delete(id).subscribe(res => {
+         this.starships = this.starships.filter(item => item.id !== id);
+         console.log('La nave ha sido eliminada, almirante.');
+    })
   }
 
 }
