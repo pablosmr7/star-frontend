aded6e78081f34fcd1f1c9e007214ddd5e9c3a4e
Pablo S <pablosmr7@gmail.com>
2022-05-05 09:43:35 +0200

    Modulos y rutas creadas

--- src/app/starship/starship-routing.module.ts
+++ src/app/starship/starship-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+
+const routes: Routes = [
+  { path: 'starship', redirectTo: 'starship/index', pathMatch: 'full'},
+  { path: 'starship/index', component: IndexComponent },
+  { path: 'starship/create', component: CreateComponent },
+  { path: 'starship/edit/:id_pilot', component: EditComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/starship/starship.module.ts
+++ src/app/starship/starship.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { StarshipRoutingModule } from './starship-routing.module';
 import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, CreateComponent, EditComponent],
   imports: [
     CommonModule,
     StarshipRoutingModule
