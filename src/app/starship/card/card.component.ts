e18bbc585119dc2f43574a1f0b4d67d28248a5fc
Pablo S <pablosmr7@gmail.com>
2022-05-30 09:30:47 +0200

    navbar responsive con mediaquery

--- src/app/starship/card/card.component.ts
+++ src/app/starship/card/card.component.ts
@@ -36,9 +36,13 @@ export class CardComponent implements OnInit {
   //AQUI VA EL CONTROL DEL FORMULARIO PARA SUBIR PILOTOS, Y ES DONDE SE ASIGNA AUTO EL this.starship.id//
 
   ngOnInit(): void {
+
+    var base=true;
+
     //console.log("enganchao mal");
     this.starshipService.getPilotShipbyId(this.starship.id).subscribe((data: PilotShip[])=>{
       this.pilotShips = data;
+
     });
 
     this.form = this.fb.group({
@@ -81,6 +85,38 @@ export class CardComponent implements OnInit {
       this.pilotShips = this.pilotShips.filter(item => item.id !== id);
       alert('El piloto ha sido eliminado, almirante');
     });
+  }
+
+
+
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////// CAMBIO DE BASE DINAMICO ///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  changeBase(){
+
+    
+
+
+
+  };
+
+  convertBase(value, from_base, to_base) {
+    var range = '0123456789\u00DF\u00DE\u00A2\u00B5\u00B6+/'.split('');
+    var from_range = range.slice(0, from_base);
+    var to_range = range.slice(0, to_base);
+
+    var dec_value = value.split('').reverse().reduce(function (carry, digit, index) {
+      if (from_range.indexOf(digit) === -1) throw new Error('Invalid digit `'+digit+'` for base '+from_base+'.');
+      return carry += from_range.indexOf(digit) * (Math.pow(from_base, index));
+    }, 0);
+    
+    var new_value = '';
+    while (dec_value > 0) {
+      new_value = to_range[dec_value % to_base] + new_value;
+      dec_value = (dec_value - (dec_value % to_base)) / to_base;
+    }
+    return new_value || '0';
   }
 
 
